a3513a35713a40956eea4506314831c74fe8b354
Yire Morlans <yire.m@icloud.com>
2023-03-14 14:06:45 -0400

    refactor: naming convention and imports for exercises

---
+++ part1/reactupp-courseinfo/src/components1/Header.jsx
@@ -0,0 +1,9 @@
+//React functional component
+const Header = ({ course }) => {
+    return (
+        <header>
+            <h1>{course}</h1>
+        </header>
+    )
+}
+export default Header
---
+++ part1/reactupp-courseinfo/src/components1/Part.jsx
@@ -0,0 +1,10 @@
+const Part = ({name, exercises}) => {
+    return (
+        <>
+            <h3>{name}</h3>
+            <p>{exercises}</p>
+        </>
+        )
+}
+
+export default Part
